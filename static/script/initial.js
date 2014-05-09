2d1afe1b15bbf6f412da77cb366b8a389be6ebb9
Travis <jaeyoung@monodiary.net>
2014-05-09 21:20:16 +0000

    Renew site contents

---
+++ static/script/initial.js
@@ -0,0 +1,38 @@
+$(window).load(function () {
+  // initial script for start isotope
+  var $container = $('div.members');
+  var $category = $('div.category');
+
+  $container.isotope({
+    itemSelector: 'div.member',
+    layoutMode: 'masonry'
+  });
+
+  // listener for filtering
+  $('button.pure-button').click(function () {
+    var filterName = $(this).attr('data-category');
+    var swc = ($(this).hasClass('active')) ? false : true;
+    filterBy(filterName, swc);
+
+    var selector = refreshFilter($category);
+    $container.isotope({filter: selector});
+  });
+});
+
+function filterBy(name, swc) {
+  var btns = $('button[data-category="' + name + '"]');
+  if (swc) {
+    btns.addClass('active');
+  } else {
+    btns.removeClass('active');
+  }
+}
+
+function refreshFilter($category) {
+  var onFilter = $category.find('button.active');
+  var filterList = '';
+  onFilter.each(function () {
+    filterList += ' .' + $(this).attr('data-category');
+  });
+  return $.trim(filterList);
+}
